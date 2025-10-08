938995db02f5767d36e5a1121fa04826adc7bf6d
samegi <sara.mejiag@javeriana.edu.co>
2025-10-08 08:40:03 -0500

    Added files from spring initializr

---
+++ src/main/java/com/proyecto/demo/PlanificadorMenuApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanificadorMenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanificadorMenuApplication.class, args);
+	}
+
+}
